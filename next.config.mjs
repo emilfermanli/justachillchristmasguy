39a439b1f9c2066ccf64432cd4acc31732ad3299
emilfermanli <emilfermanli@gmail.com>
2024-11-20 22:50:53 +0400

    beta site

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/justachillchristmasguy/" : "",
-  basePath: isProd ? "/justachillchristmasguy" : "",
+  assetPrefix: "/justachillchristmasguy/",
+  basePath: "/justachillchristmasguy",
   output: "export",
 };
 
